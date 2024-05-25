fdae723d05810626c7807fe724d8c9a15c86209e
Kravets <kravets.inside@gmail.com>
2024-05-25 19:10:51 +0200

    1

--- scripts/touch.js
+++ scripts/touch.js
@@ -8,22 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
 		)
 	}
 
-	// Добавление класса 'touch-device' к <body>, если устройство является тач-устройством
-	if (isTouchDevice()) {
-		document.body.classList.add('touch-device')
-	}
-
 	// Проверка, является ли устройство тач-устройством
-	if (document.body.classList.contains('touch-device')) {
-		// Для тач-устройств, заменяем :hover на :active
-		const hoverElements = document.querySelectorAll('.hover-effect')
-		hoverElements.forEach(element => {
-			element.addEventListener('touchstart', () => {
-				element.classList.add('hover-active')
-			})
-			element.addEventListener('touchend', () => {
-				element.classList.remove('hover-active')
-			})
-		})
+	if (isTouchDevice()) {
+		// Добавление класса 'touch-device' к <body>, если устройство является тач-устройством
+		document.body.classList.add('touch-device')
 	}
 })
